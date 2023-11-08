f5e4dcc44e85bd9a980ba33d847b03b713a99d85
LeTöm <t.buser21@gmail.com>
2023-11-08 13:48:09 +0100

    Fix vercel deployment error

--- src/lib/components/AuthForm.svelte
+++ src/lib/components/AuthForm.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
-    import * as Card from "$lib/components/ui/card";
-    import logo from "$lib/assets/logo.png";
 
     import { login } from "$lib/msal";
 </script>
