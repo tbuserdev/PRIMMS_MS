fadf9c0dbf428667f405aaa50c2d63bf9c314fc5
LeTöm <t.buser21@gmail.com>
2023-11-16 17:00:02 +0100

    Add .csv UI

--- src/routes/(forms)/start/+page.svelte
+++ src/routes/(forms)/start/+page.svelte
@@ -9,6 +9,7 @@
     import { Button } from "$lib/components/ui/button";
     import { Input } from "$lib/components/ui/input";
     import { Label } from "$lib/components/ui/label";
+    import { get } from "svelte/store";
     
     async function updateUserStore(){
         if ($page.form) {
@@ -34,21 +35,58 @@
         }
     }
 
+    async function upload() {
+        // TODO: Upload
+        // https://www.okupter.com/blog/sveltekit-file-upload
+    }
+
+    async function download() {
+        const filename = get(authStore).name + '.csv';
+
+        const data = get(userStore);
+        const csv = Object.keys(data).map(key => `${key},${data[key]}`).join('\n');
+        const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        link.style.visibility = 'hidden';
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     if ($page.form) {
         updateUserStore();
     }
 
+    let input = "";
+
+    $: console.log(input)
     console.log($page.form);
     console.log($userStore);
 
-    // TODO: Implement download of csv file
-    // TODO: OneDrive Connection
+    // TODO: OneDrive Link
 </script>
 
-<section class="space-y-10">
+<section class="space-y-10 w-full">
     <div class="space-y-1">
         <h3 class="text-2xl font-semibold tracking-tight">Hallo {$authStore.name}!</h3>
         <p class="text-sm text-muted-foreground mb-10">Willkommen auf der Startseite des IT-Portals.</p>
+    </div>
+
+
+    <div class="items-center gap-3 space-y-2">
+        <Label for="data">CSV</Label>
+        <Input id="data" type="file" accept=".csv," bind:value={input}/>
+        {#if input != ""}
+            <Button on:click={upload} class="w-full mt-2">Hochladen</Button>
+        {:else}
+            <Button disabled class="w-full mt-2">Hochladen</Button>
+        {/if}
     </div>
 
     <div>
@@ -70,7 +108,8 @@
                 {/if}
              {/each}
             <input type="text" id="accessKey" name="accessKey" class="hidden" bind:value={$authStore.accessToken}>
-            <Button>Persönliche Informationen updaten</Button>
+            <Button variant="outline">Persönliche Informationen updaten</Button>
         </form>
+        <Button on:click={download} class="w-full mt-4">Persönliche Informationen herunterladen</Button>
     </div>
 </section>
