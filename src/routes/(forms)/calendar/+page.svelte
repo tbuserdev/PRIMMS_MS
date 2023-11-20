d664586ce41ef3e51098fb21a2351e6f2099d34a
LeTöm <t.buser21@gmail.com>
2023-11-20 17:14:18 +0100

    Update Todos

--- src/routes/(forms)/calendar/+page.svelte
+++ src/routes/(forms)/calendar/+page.svelte
@@ -11,20 +11,13 @@
 	export let data;
 
 	// TODO: Error message from server and sucess form submit
+	// TODO: Easier CSV Upload - copy from startpage
 </script>
 
 <div class="space-y-6">
 	<div>
 		<h3 class="text-lg font-medium">Kalender Update</h3>
 		<p class="text-sm text-muted-foreground">Hier kannst du automatisch deinen Kalender updaten</p>
-		<Accordion.Root class="mt-2">
-			<Accordion.Item value="item-1">
-				<Accordion.Trigger>Wie funktionierts?</Accordion.Trigger>
-				<Accordion.Content>
-				1. .csv File
-				</Accordion.Content>
-			</Accordion.Item>
-			</Accordion.Root>
 	</div>
 	<BirthdayForm form={data.form} accessToken={$authStore.accessToken} />
 </div>
